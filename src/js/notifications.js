fee2dacd98030f9b78a6f61e3b4a3641a709f02d
Boris Leistedt <boris.leistedt@gmail.com>
2018-08-09 17:57:11 +0200

    Changed name

--- src/js/notifications.js
+++ src/js/notifications.js
@@ -9,7 +9,7 @@ class Notifications {
 
 	createBrowserNotification(timerType) {
 
-		let quotes= Array('You can do it!','Do not give up', 'You is perfect', 'You is kind', 'You is beautiful', 'If not us, who?','If not now, when?','Fear is absence of love.', 'A pint filled drop by drop', 'Every moment is a fresh beginning', 'The best way is always through', 'You better werk','We are born naked and the rest is Drag', 'We all have our pupropses, we all have our strengths', 'Mistakes make you who you are','This is the beginning of the rest of your life', 'And If I fly or if I fall, at least I can say I gave it all.');
+		let quotes= Array('You can do it!','Do not give up', 'You is perfect', 'You is kind', 'You is beautiful', 'If not us, who?','If not now, when?','Fear is absence of love.', 'A pint filled drop by drop', 'Every moment is a fresh beginning', 'The best way is always through', 'You better werk','We are born naked and the rest is Drag', 'We all have our pupropses, we all have our strengths', 'Mistakes make you who you are','This is the beginning of the rest of your life', 'And If I fly or if I fall, at least I can  say I gave it all.');
 
 		let message = quotes[Math.floor(Math.random()*quotes.length)];
 
@@ -27,7 +27,7 @@ class Notifications {
 		browser.notifications.create(NOTIFICATION_ID, {
 			type: 'basic',
 			iconUrl: '/assets/img/tomato-icon-64.png',
-			title: 'Tomato Clock',
+			title: 'Motivator',
 			message
 		});
 
